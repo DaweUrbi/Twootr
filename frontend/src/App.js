afce99412f1a82c356bfd91e87f3d4b67a0c21d7
Misato Tanno <breitmeer.0804@gmail.com>
2022-08-05 15:17:14 -0700

    changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,32 @@
 import {useState} from 'react';
+import axios from 'axios';
+import {useEffect} from 'react';
 import './App.css';
+import './components/MyTwoot.module.css'
 
 import Navbar from './components/Navbar.js';
 import Header from './components/Header.js';
 import Twoots from "./components/Twoots";
+import Twoot from './components/Twoot.js';
 
-function App() {
+function App(props) {
+
+  const [twoots, setTwoots] = useState([])
+  const DATA_URL = "http://localhost:8080/twoots";
+
+  const getTwoots = () => {
+    axios.get(DATA_URL)
+      .then(response => {
+        setTwoots(response.data)
+        console.log(response.data)
+      })
+  }
+
+  useEffect(() => {
+    getTwoots();
+  }, [])
+
+
   const [parentName, setParentName] = useState('David');
   const [parentSurname, setParentSurname] = useState('Urban');
   const [parentNick, setParentNick] = useState('david-urban');
@@ -20,7 +41,15 @@ function App() {
               nick={parentNick}
               updateNick={setParentNick}
       ></Header>
-      < Twoots />
+      <Twoot
+        author={parentName + " " + parentSurname}
+        authorSlug={parentNick}
+        twoots={twoots}
+        setTwoots={setTwoots}
+      />
+      < Twoots
+        twoots={twoots}
+      />
     </div>
   );
 }
